b423248c0ef5555a9f18b5a56e83b2262c5af57f
stiartsly <stiartsly@gmail.com>
2019-03-21 11:39:21 +0800

    added shorthand script to build libcrystal

---
+++ compile.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cmake -DCMAKE_INSTALL_PREFIX=dist \
+      -DENABLE_CRYPTO=ON \
+      -DENABLE_TESTS=ON \
+      -DWITH_LIBCUNIT=${DEPSDIR} \
+      -DWITH_LIBSODIUM=${DEPSDIR} \
+      -DLIBSODIUM_STATIC=ON \
+      ..
+
